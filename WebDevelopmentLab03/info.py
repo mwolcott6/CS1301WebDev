deb67501106aefd9f2a81ca718218ef68876f4bd
Helen-42 <68658693+Helen-42@users.noreply.github.com>
2024-11-22 14:56:06 -0500

    Add files via upload

---
+++ WebDevelopmentLab03/info.py
@@ -0,0 +1,82 @@
+
+#This File will contain the information to be displayed in your portfolio
+
+#CHANGE BELOW
+profile_picture = "Images/profile.jpeg"
+about_me = "My name is Helen Honecker. I am a freshman at Georgia Tech, studying Computer Science"
+
+
+#CHANGE BELOW (OPTIONAL)
+linkedin_image_url = "https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"
+github_image_url = "https://cdn-icons-png.flaticon.com/256/25/25231.png"
+email_image_url = "https://logowik.com/content/uploads/images/513_email.jpg"
+
+#CHANGE BELOW
+my_linkedin_url = "https://www.linkedin.com/in/helen-honecker-355332328/"
+my_github_url = "https://github.com/Helen-42"
+my_email_address = "hhonecker3@gatech.edu"
+
+
+education_data ={
+    'Degree': 'Bachelor of Science in Computer Science',
+    'Institution': 'Georgia Institute of Technology', 
+    'Location': 'Atlanta, GA',
+    'Objective Statement': 'Motivated computer science student passionate about utilizing data collection and analysis to advance scientific discovery in the field of space research. Interested in research and internship positions.',
+    'Graduation Date': '2028',
+    'GPA': '4.0'
+}
+course_data = {
+    "code":["CS 1301", "MATH 1551", "Eng 1102", "PUBP 1142",], 
+    "names":["Intro to CS", "Differential Calculus", "English Composition", "Teams and Collaboration",], 
+    "semester_taken":["1st", "1st", "1st", "1st"],
+    "skills":["Python ", "OOP(S!) (Java Edition)", "Basically row reduction", "If only my grade was discrete too..."],
+    }
+experience_data = {
+    "CS Student Ambassador (Google & National 4-H Council)": (["- Worked on designing the Krabby Patty Recipe",
+                                                                          "- Increased Company Revenue by 200 dollars", "- Made Mr. Krabs Proud"],"Images/cook.jpg"),
+    "Greenbank Observatory Researcher":(["- Made the floor almost reflective",
+                                                           "- Increased customer visits by three folds by making restaurant clean"],"Images/cleaner.jpg"),
+    "Jelly Fish Catcher":(["- Caught 100+ Jellyfish with the help of my colleague Patrick"],"Images/jelly.jpg")
+
+}
+
+projects_data = {
+    "GC Acid Mine Drainage Model": "Created the famous Recipe and tailored it from scratch",
+}
+
+programming_data = {
+    "Python": 90,
+    "Java": 65,
+    "LabVIEW": 80,
+}
+
+#CHANGE BELOW (OPTIONAL)
+programming_icons = {
+    "Python": "🐍",
+    "Java": "☕",
+    "LabVIEW": "🔍",
+}
+spoken_icons = {
+    "English": "🇬🇧",
+    "Spanish":"🇪🇸"
+}
+
+#CHANGE BELOW
+spoken_data = {
+    "English": "Fluent",
+    "Spanish": "Proficient",
+}
+leadership_data = {
+    "President of MARS HoF FRC 501c3 Teams": (["- Elected Worst Student of the Century"],"Images/puff.jpg"),
+    "Creator/Manager of Appalachia Live Conference": (["- Elected Worst Student of the Century"],"Images/puff.jpg"),
+
+
+}
+activity_data={
+    "University High School Orchestra": ["- Conviced Squidwards to have fun", 
+            "- Sat under Patrick's rock"]
+    "UHS Varisty Track and Field": ["- Conviced Squidwards to have fun", 
+            "- Sat under Patrick's rock"]
+    "UHS Varsity Cross Country": ["- Conviced Squidwards to have fun", 
+            "- Sat under Patrick's rock"]
+}
